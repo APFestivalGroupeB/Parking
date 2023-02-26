6412abd327d871e90f21305bedb445d2a7d6dc13
bkasteller <marie.ucles@neuf.fr>
2023-02-26 15:35:24 +0100

    Ajout d'un admin (seeder)

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('utilisateur')->insert([
+            'nom' => 'Super',
+            'prenom' => 'Admin',
+            'telephone' => null,
+            'email' => 'admin@parking.com',
+            'password' => Hash::make('secret'),
+            'est_valide' => true,
+            'est_admin' => true,
+            'position' => null,
+        ]);
+    }
+}
