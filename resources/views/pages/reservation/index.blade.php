ef60efc005d6e7cd453a1d0dd4137e41810b5048
Mariecls <marie.ucles@neuf.fr>
2023-02-28 13:15:53 +0100

    Début index

--- resources/views/pages/reservation/index.blade.php
+++ resources/views/pages/reservation/index.blade.php
@@ -1,1 +1,25 @@
 reservation.index
+
+@section('content')
+<div class="section-card">
+    <div class="section-card__header">
+        <div>
+            <h1 class="h4 text-nowrap">Reservation</h1>
+        </div>
+
+        <ul class="header-options">
+            <li>
+                <a class="btn btn-primary">
+                    <i class="bi bi-plus"></i>
+                </a>
+            </li>
+       
+        </ul>
+    </div>
+
+    <div class="section-card__body">
+  
+    </div>
+</div>
+@endsection
+
