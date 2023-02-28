9b429fe6e37c696d4f4a64875109418978178cdf
Mariecls <marie.ucles@neuf.fr>
2023-02-28 13:36:28 +0100

    Create place

--- resources/views/pages/place/create.blade.php
+++ resources/views/pages/place/create.blade.php
@@ -16,9 +16,7 @@ place.create
                         <div class="form-floating col">
                             <input class="form-control @error('num_place') is-invalid @enderror" type="text" name="num_place" placeholder=" " value="{{ old('num_place') }}">
                             <label>Numero place</label>
-                            @error('num_place')
-                            <div class="form-text text-danger">{{ $message }}</div>
-                            @enderror
+                            
                         </div>
                    
              
@@ -27,10 +25,7 @@ place.create
                             <div class="form-floating">
                                 <input class="form-control @error('place_id') is-invalid @enderror" type="text" name="place_id" placeholder=" " value="{{ old('place_id') }}">
                                 <label>Id place</label>
-                                @error('place_id)
-                                <div class="form-text text-danger">{{ $message }}</div>
-                                @enderror
-                            </div>
+                              
                     </div>
                        
 
@@ -41,19 +36,4 @@ place.create
     </form>
 </div>
 
-                        <section class="container">
-  <h2 class="py-2">Datepicker in Bootstrap 5</h2>
-  <form class="row">
-    <label for="date" class="col-1 col-form-label">Date</label>
-    <div class="col-5">
-      <div class="input-group date" id="datepicker">
-        <input type="text" class="form-control" id="date"/>
-        <span class="input-group-append">
-          <span class="input-group-text bg-light d-block">
-            <i class="fa fa-calendar"></i>
-          </span>
-        </span>
-      </div>
-    </div>
-  </form>
-</section>
+                       
