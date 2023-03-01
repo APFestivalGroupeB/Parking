b5a8a4484abd2f470f15a73143276a0687ea922c
Mariecls <marie.ucles@neuf.fr>
2023-03-01 15:34:14 +0100

    début index place

--- resources/views/pages/place/index.blade.php
+++ resources/views/pages/place/index.blade.php
@@ -1,1 +1,34 @@
-place.index
+
+@extends('layouts.app')
+
+
+
+
+@section('content')
+
+
+<div class="section-card">
+    <div class="section-card__header">
+        <div>
+            <h1 class="h4 text-nowrap">Place</h1>
+            <small class="text-muted">Dates: </small>
+        </div>
+        <ul class="header-options">
+           
+            <li>
+                <a class="btn btn-primary" href="{{ route('place.create') }}">
+                    <i class="bi bi-plus-square-fill"></i>
+                </a>
+            </li>
+       
+        </ul>
+    </div>
+
+    
+</div>
+
+
+
+@endsection
+
+
