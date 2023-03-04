638321178ba75401e8c39b7bf269c702992f9f87
Mariecls <marie.ucles@neuf.fr>
2023-03-04 15:11:03 +0100

    numero defaut

--- database/migrations/2023_02_26_140320_create_place_table.php
+++ database/migrations/2023_02_26_140320_create_place_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('place', function (Blueprint $table) {
             $table->id();
-            $table->string('numero');
+            $table->string('numero')->default('');
             $table->timestamps();
         });
     }
